542913be7384be3564ce6b6378a1f4a86e48405b
annnie73 <aniaszymik02@gmail.com>
2025-03-10 11:04:38 +0100

    modify data downloading script

--- get_data.sh
+++ get_data.sh
@@ -1,9 +1,11 @@
+mkdir data
+
 # FASTA files with DNA sequences of genomic regions from train/val/test sets
-wget 'https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_Train.fa'
-wget 'https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_Val.fa'
-wget 'https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_Test.fa'
+wget https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_Train.fa -O data/Sequences_Train.fa
+wget https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_Val.fa -O data/Sequences_Val.fa
+wget https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_Test.fa -O data/Sequences_Test.fa
 
 # Files with developmental and housekeeping activity of genomic regions from train/val/test sets
-wget 'https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_activity_Train.txt'
-wget 'https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_activity_Val.txt'
-wget 'https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_activity_Test.txt'
+wget https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_activity_Train.txt -O data/Sequences_activity_Train.txt
+wget https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_activity_Val.txt -O data/Sequences_activity_Val.txt
+wget https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_activity_Test.txt -O data/Sequences_activity_Test.txt
