a9da7111ad14c005851bf60ebcee8e39a201cdea
annnie73 <aniaszymik02@gmail.com>
2025-03-31 11:19:07 +0200

    add padding as a parameter

--- models.py
+++ models.py
@@ -7,19 +7,19 @@ class DeepSTARR(nn.Module):
         super(DeepSTARR, self).__init__()
         
         self.conv1 = nn.Conv1d(in_channels=4, out_channels=params['num_filters'],
-                               kernel_size=params['kernel_size1'], padding='same')
+                               kernel_size=params['kernel_size1'], padding=params['pad'])
         self.bn1 = nn.BatchNorm1d(params['num_filters'])
         
         self.conv2 = nn.Conv1d(in_channels=params['num_filters'], out_channels=params['num_filters2'],
-                               kernel_size=params['kernel_size2'], padding='same')
+                               kernel_size=params['kernel_size2'], padding=params['pad'])
         self.bn2 = nn.BatchNorm1d(params['num_filters2'])
         
         self.conv3 = nn.Conv1d(in_channels=params['num_filters2'], out_channels=params['num_filters3'],
-                               kernel_size=params['kernel_size3'], padding='same')
+                               kernel_size=params['kernel_size3'], padding=params['pad'])
         self.bn3 = nn.BatchNorm1d(params['num_filters3'])
         
         self.conv4 = nn.Conv1d(in_channels=params['num_filters3'], out_channels=params['num_filters4'],
-                               kernel_size=params['kernel_size4'], padding='same')
+                               kernel_size=params['kernel_size4'], padding=params['pad'])
         self.bn4 = nn.BatchNorm1d(params['num_filters4'])
         
         self.pool = nn.MaxPool1d(kernel_size=2)
@@ -30,7 +30,7 @@ class DeepSTARR(nn.Module):
         self.fc2 = nn.Linear(params['dense_neurons1'], params['dense_neurons2'])
         self.bn_fc2 = nn.BatchNorm1d(params['dense_neurons2'])
         
-        # heads per task (developmental and housekeeping enhancer activities)
+        # Heads per task (developmental and housekeeping enhancer activities)
         self.fc_dev = nn.Linear(params['dense_neurons2'], 1)
         self.fc_hk = nn.Linear(params['dense_neurons2'], 1)
         
